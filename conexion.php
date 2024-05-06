c15d1e51c2da4b5c7c3f9a87650c1e5accbb2e80
Esau <esau.vazquez@durango.gob.mx>
2024-05-06 10:31:41 -0600

    conexion segura

--- conexion.php
+++ conexion.php
@@ -1,12 +1,20 @@
 <?php
+// Configuración de la conexión
+$hostname = "localhost";
+$username = "tu_usuario";
+$password = "tu_contraseña";
+$database = "android_mysql";
 
-$mysql = new mysqli("localhost", "root","", "android_mysql");
+// Crear una nueva instancia de mysqli
+$mysqli = new mysqli($hostname, $username, $password, $database);
 
-if($mysql->connect_error){
-    die("Error de conexión");
+// Verificar la conexión
+if ($mysqli->connect_errno) {
+    // Ocurrió un error al conectar
+    error_log("Error al conectar a la base de datos: " . $mysqli->connect_error);
+    die("Error de conexión. Por favor, inténtalo de nuevo más tarde.");
+} else {
+    // La conexión fue exitosa
+    // Puedes realizar operaciones con la base de datos aquí
 }
-else{
-    //echo "Conexión exitosa";
-}
-
 ?>
