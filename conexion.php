5be1ba3683acb5b6f8fca5a0e773714c8353b563
Esau <esau.vazquez@durango.gob.mx>
2024-05-06 10:29:51 -0600

    conexion default

---
+++ conexion.php
@@ -0,0 +1,12 @@
+<?php
+
+$mysql = new mysqli("localhost", "root","", "android_mysql");
+
+if($mysql->connect_error){
+    die("Error de conexión");
+}
+else{
+    //echo "Conexión exitosa";
+}
+
+?>
