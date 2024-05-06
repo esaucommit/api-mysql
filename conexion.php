6805f3a342f966998f6b58bbfd6550cf5c48ab4e
Esau <esau.vazquez@durango.gob.mx>
2024-05-06 10:45:20 -0600

    try catch en conexion

--- conexion.php
+++ conexion.php
@@ -3,18 +3,24 @@
 $hostname = "localhost";
 $username = "root";
 $password = "";
-$database = "android_mysql";
+$database = "android_mysq";
+// Intenta realizar la conexión
+try {
+    // Crear una nueva instancia de mysqli
+    $mysqli = new mysqli($hostname, $username, $password, $database);
 
-// Crear una nueva instancia de mysqli
-$mysqli = new mysqli($hostname, $username, $password, $database);
-
-// Verificar la conexión
-if ($mysqli->connect_errno) {
-    // Ocurrió un error al conectar
-    error_log("Error al conectar a la base de datos: " . $mysqli->connect_error);
-    die("Error de conexión. Por favor, inténtalo de nuevo más tarde.");
-} else {
-    // La conexión fue exitosa
-    // Puedes realizar operaciones con la base de datos aquí
+    // Verificar si hubo un error durante la conexión
+    if ($mysqli->connect_errno) {
+        // Ocurrió un error al conectar
+        throw new Exception("Error al conectar a la base de datos: " . $mysqli->connect_error);
+    } else {
+        // La conexión fue exitosa
+        // Puedes realizar operaciones con la base de datos aquí
+        echo "Conexión exitosa";
+    }
+} catch (Exception $e) {
+    // Manejar la excepción
+    error_log($e->getMessage()); // Registrar el error en el archivo de registro
+    die("Error al conectar a la base de datos. Por favor, inténtalo de nuevo más tarde.");
 }
 ?>
